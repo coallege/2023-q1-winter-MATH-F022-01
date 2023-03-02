ceb37160483407318969bf0d5adb98385b029e24
coalpha <22552895+coalpha@users.noreply.github.com>
2023-03-01 18:59:39 -0800

    the graph was bipartite

--- the-bipartite-mind/the-bipartite-mind.hs
+++ the-bipartite-mind/the-bipartite-mind.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE BlockArguments #-}
 module Main(main) where
+import qualified Data.Set as Set
 
 parseEntry :: String -> Maybe (String, String)
 parseEntry s =
@@ -8,24 +9,49 @@ parseEntry s =
          then Just (head words', head $ tail words')
          else Nothing
 
-data Side = LeftSide | RightSide
+takeLines :: IO [(String, String)]
+takeLines = do
+   line <- getLine
+   if line == "done"
+      then return []
+      else do
+         let parsed = parseEntry line
+         case parsed of
+            Nothing -> do
+               putStrLn "Could not understand that input!"
+               takeLines
+            Just iceForAll -> do
+               rest <- takeLines
+               return $ iceForAll : rest
 
-main :: IO()
+twonarySort :: [(String, String)] -> IO Bool
+twonarySort entireList = let
+   recursiveSort leftSide rightSide lst =
+      if null lst
+         then return True
+         else let
+               (a, b) = head lst
+               rest   = tail lst
+               aleft  = Set.member a leftSide
+               bleft  = Set.member b leftSide
+               aright = Set.member a rightSide
+               bright = Set.member b rightSide
+            in if aleft && bleft
+               then do
+                  putStrLn $ a ++ " was sorted left but it is paired with " ++ b ++ " which was also sorted left."
+                  return False
+               else if aright && bright
+                  then do
+                     putStrLn $ a ++ " was sorted right but it is paired with " ++ b ++ " which was also sorted right."
+                     return False
+                  else if (aright || bleft)
+                     then recursiveSort (Set.insert b leftSide) (Set.insert a rightSide) rest
+                     else recursiveSort (Set.insert a leftSide) (Set.insert b rightSide) rest
+   in recursiveSort Set.empty Set.empty entireList
+
+main :: IO ()
 main = do
    putStr "Enter an adjacency list with one entry per line separated by spaces:\nType 'done' when you're finished.\n"
-   let
-      takeLine = do
-         line <- getLine
-         if line == "done"
-            then return []
-            else do
-               let parsed = parseEntry line
-               case parsed of
-                  Nothing -> do
-                     putStrLn "Could not understand that input!"
-                     takeLine
-                  Just iceForAll -> do
-                     rest <- takeLine
-                     return $ iceForAll : rest
-   pairs <- takeLine
-   return ()
+   pairs <- takeLines
+   bipartite <- twonarySort pairs
+   putStrLn $ "The graph " ++ (if bipartite then "was" else "was not") ++ " bipartite."
